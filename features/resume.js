4f6595a981d8145d07107fd5caf493a76ae36f09
Ethan Wright <wrightet707@gmail.com>
2020-10-26 14:53:38 -0700

    add linkedin and portfolio

--- features/resume.js
+++ features/resume.js
@@ -296,6 +296,12 @@ module.exports = function (controller) {
     controller.hears('github', 'message,direct_message', async(bot, message) => {
         await bot.reply(message, "https://github.com/wrightet")
     })
+    controller.hears('portfolio', 'message,direct_message', async(bot, message) => {
+        await bot.reply(message, "https://wrightet.github.io/ethan-wright/")
+    })
+    controller.hears('linkedin', 'message,direct_message', async(bot, message) => {
+        await bot.reply(message, "https://www.linkedin.com/in/ethan-wright-91a75270/")
+    })
     controller.hears(['^what is your favorite food$'], 'message,direct_message', async(bot, message) => {
         await bot.reply(message, "Ethan's favorite food is chocolate chip cookies.")
     })
